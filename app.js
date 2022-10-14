9027a77c05e8b03b9e7d3163649d67b700bb4579
sem oktavianus <sem.oktavianus@gmail.com>
2022-10-15 06:20:28 +0700

    finishing button function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const label = document.querySelector(".label");
 const slider = document.querySelector("#slider");
 const canvas = document.querySelector(".canvas");
 const colorPic = document.querySelector("#color-pic");
+const btnContainer = document.querySelector(".btn-container");
+const buttons = document.querySelectorAll(".button");
+const button = document.querySelector(".button");
 
 for (let i = 1; i <= 100; i++) {
   const pixel = document.createElement("div");
@@ -23,3 +26,15 @@ const onChangeSlider = () => {
   label.textContent = `${slider.value} X ${slider.value}`;
 };
 slider.addEventListener("change", onChangeSlider);
+//..............
+const onClickButton = (e) => {
+  const siblings = btnContainer.children;
+  for (let sibling of siblings) {
+    sibling.classList.remove("active");
+  }
+  e.target.classList.add("active");
+};
+
+for (let button of buttons) {
+  button.addEventListener("click", onClickButton);
+}
