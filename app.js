7d3eef5fb41aee1b60201b550108538725740efa
sem oktavianus <sem.oktavianus@gmail.com>
2022-10-15 09:46:45 +0700

    fix eraser function

--- app.js
+++ app.js
@@ -6,27 +6,51 @@ const btnContainer = document.querySelector(".btn-container");
 const buttons = document.querySelectorAll(".button");
 const button = document.querySelector(".button");
 
-for (let i = 1; i <= 100; i++) {
-  const pixel = document.createElement("div");
-  pixel.setAttribute("class", "pixel");
-  pixel.setAttribute("id", `pixel-${i}`);
-
-  canvas.appendChild(pixel);
-}
-const pixels = document.querySelectorAll(".pixel");
-const draw = (e) => {
-  e.target.setAttribute("style", `background-color: ${colorPic.value}`);
-  console.log("click");
+//creating function canvas resolution
+const resolution = (numPxl) => {
+  for (let i = 1; i <= numPxl * numPxl; i++) {
+    const pixel = document.createElement("div");
+    pixel.setAttribute("class", "pixel");
+    pixel.setAttribute("id", `pixel-${i}`);
+    canvas.appendChild(pixel);
+    canvas.setAttribute(
+      "style",
+      `grid-template-columns: repeat(${numPxl}, 1fr); grid-template-rows: repeat(${numPxl}, 1fr) ;`
+    );
+  }
 };
-pixels.forEach((element) => {
-  element.addEventListener("click", draw);
-});
+resolution(slider.value);
+
+//drawing function
+const drawing = (color) => {
+  const pixels = document.querySelectorAll(".pixel");
+  const draw = (e) => {
+    e.target.setAttribute("style", `background-color: ${color}`);
+  };
+  for (let pixel of pixels) {
+    pixel.addEventListener("click", draw);
+    console.log("click");
+  }
+};
+
+drawing(colorPic.value);
+
+//clear function
+const clear = () => {
+  canvas.innerHTML = "";
+  resolution(slider.value);
+  drawing(colorPic.value);
+};
+
+//set resolution with slider input
 
 const onChangeSlider = () => {
   label.textContent = `${slider.value} X ${slider.value}`;
+  clear();
 };
 slider.addEventListener("change", onChangeSlider);
-//..............
+
+//click button function
 const onClickButton = (e) => {
   const siblings = btnContainer.children;
   for (let sibling of siblings) {
